400be376bb18dc442b3aa24543c9c36fddc0b90d
Christopher Young <christopheryoung@MacBook123.local>
2024-11-21 21:26:54 -0800

    added pagination

--- src/pages/projects/ProjectsList.tsx
+++ src/pages/projects/ProjectsList.tsx
@@ -1,4 +1,8 @@
 import "./ProjectsList.scss";
+// React
+import { useState, useEffect } from "react";
+// Hooks
+import usePagination from "../../hooks/usePagination";
 // Types
 import { IProject } from "../../types/index.ds";
 // Data
@@ -9,12 +13,34 @@ interface ProjectsListProps {
   section2Ref: React.MutableRefObject<HTMLDivElement | null>;
 }
 
+type pageDir = "prev" | "next";
+
 const ProjectsList: React.FC<ProjectsListProps> = ({setShowProject, section2Ref}) => {
+  // Pagination
+  const [pageContent, setPageContent] = useState<IProject[] | null>(null);
+  // Hooks
+  const pagi = usePagination(projectsData, 6);
+
+  useEffect(() => {
+    let temp = pagi.currentData();
+    setPageContent(temp);
+  }, [pagi.currentPage]);
+
   const toggleMode = (project: IProject) => {
     setShowProject(project);
     // Scroll to projects-segment
     if(section2Ref.current) section2Ref.current.scrollIntoView();
   };
+
+  const handlePagi = (pageDir: pageDir) => {
+    if(pageDir === "prev") {
+      pagi.prevPage();
+    } else {
+      pagi.nextPage();
+    }
+    // Scroll to projects-segment
+    if(section2Ref.current) section2Ref.current.scrollIntoView();
+  }
 
   return(
     <div id="projectsList">
@@ -22,22 +48,32 @@ const ProjectsList: React.FC<ProjectsListProps> = ({setShowProject, section2Ref}
         Projects
       </div>
 
-      <div className="projectsList-projects">
-        {projectsData.map((project, idx) => (
-          <div className="projectsList-project" key={idx}>
-            <img src={project.images[0]}/>
-            <div className="projectsList-text">
-              <div className="projectsList-name">
-                {project.name}
-                {project.deployed && <span>Live</span>}
+      {pageContent &&
+        <div className="projectsList-projects">
+          {pageContent.map((project, idx) => (
+            <div className="projectsList-project" key={idx}>
+              <img src={project.images[0]}/>
+              <div className="projectsList-text">
+                <div className="projectsList-name">
+                  {project.name}
+                  {project.deployed && <span>Live</span>}
+                </div>
+                <div className="projectsList-about">{project.about}</div>
               </div>
-              <div className="projectsList-about">{project.about}</div>
+              <div className="projectsList-btns">
+                <button onClick={() => toggleMode(project)}>View</button>
+              </div>
             </div>
-            <div className="projectsList-btns">
-              <button onClick={() => toggleMode(project)}>View</button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
+      }
+
+      <div className="projectsList-pagination">
+        <div className="projectsList-pagination-content">
+          <button className="projectsList-prev" onClick={() => handlePagi("prev")}>Prev</button>
+          <div>{pagi.currentPage} / {pagi.totalPages}</div>
+          <button className="projectsList-next" onClick={() => handlePagi("next")}>Next</button>
+        </div>
       </div>
     </div>
   )
