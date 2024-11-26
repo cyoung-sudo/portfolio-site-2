52d9047a0fa5dbfbc1f12a7f6230cb5d8a1c6400
Christopher Young <christopheryoung@MacBook123.local>
2024-11-26 15:54:09 -0800

    added styling

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -1,11 +1,15 @@
 import "./NavigationBar.scss";
+// Font styles
+import { LinearGradient } from 'react-text-gradients';
 
 const NavigationBar = () => {
   return(
     <div id="navigationBar">
       <div className="navigationBar-content">
         <div className="navigationBar-brand">
-          Dev Portfolio
+          <LinearGradient gradient={['to left', 'steelblue ,black']}>
+            Dev Portfolio
+          </LinearGradient>
         </div>
       </div>
     </div>
